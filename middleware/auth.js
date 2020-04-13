4ffaf9c65db090529c718245f7aa5e5323b83cf0
Michael Oki <oki.mikael@outlook.com>
2020-04-13 15:12:09 +0700

    Membetulkan error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 var connection = require('../koneksi');
 var mysql = require('mysql');
-var md5= require('MD5');
+var md5 = require('MD5');
 var response =require('../res');
 var jwt = require('jsonwebtoken');
 var config = require('../config/secret');
@@ -8,7 +8,7 @@ var ip = require('ip');
 
 //controller untuk registrasi
 exports.registrasi = function(req, res) {
-    var post ={
+    var post = {
         username: req.body.username,
         email: req.body.email,
         password: md5(req.body.password),
@@ -26,10 +26,10 @@ exports.registrasi = function(req, res) {
             console.log(error);
         }else{
             if(rows.length == 0){
-                var query = "INSERT INTO ?? SET ??";
+                var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
                 query = mysql.format(query,table);
-                connection.query(query, function(error,rows){
+                connection.query(query, post, function(error, rows){
                     if(error){
                         console.log(error);
                     }else{
@@ -40,5 +40,5 @@ exports.registrasi = function(req, res) {
                 response.ok("Email sudah terdaftar!");
             }
         }
-    })
-}
+    });
+};
