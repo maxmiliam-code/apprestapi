dc01fe17ef7458fae16cb8b20dc3ee50e498f310
Michael Oki <oki.mikael@outlook.com>
2020-04-27 19:03:49 +0700

    berhasil menginput data dengan role 2

--- middleware/index.js
+++ middleware/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 var verifikasi = require('./verifikasi');
-var verifikasiinput = require('./verifikasiinput')
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
